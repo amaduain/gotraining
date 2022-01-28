303a9dc1911f8cb54237e8f860981564bb78c57d
Alex <amaduain@gmail.com>
2022-01-28 12:30:19 +0100

    Variable declaration

--- CARDS/main.go
+++ CARDS/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var card string
+	//card  = "Ace of Spades"
+	card := "Ace of Spades"
+	// card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
