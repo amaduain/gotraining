463440e6f715578aae435e101d32bf01866debd8
Alex <amaduain@gmail.com>
2022-01-28 12:36:28 +0100

    Functio returns

--- CARDS/main.go
+++ CARDS/main.go
@@ -5,8 +5,12 @@ import "fmt"
 func main() {
 	// var card string
 	//card  = "Ace of Spades"
-	card := "Ace of Spades"
+	card := newCard()
 	// card = "Five of Diamonds"
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
