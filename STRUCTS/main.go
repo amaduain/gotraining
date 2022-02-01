d1dac5adc39d65926d4aa3f30c096fb6624a2f80
Alex <amaduain@gmail.com>
2022-02-01 15:17:35 +0100

    Nested Structs

--- STRUCTS/main.go
+++ STRUCTS/main.go
@@ -4,14 +4,27 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	fmt.Println("Starting....")
-	me := person{firstName: "Alex", lastName: "Madurga"}
+	me := person{
+		firstName: "Alex",
+		lastName:  "Madurga",
+		contact: contactInfo{
+			email:   "amaduian@gnmail.com",
+			zipCode: 28000,
+		},
+	}
 	me.firstName = "Alex"
 	fmt.Println("Name: ", me.firstName)
+	fmt.Printf("%+v", me)
 }
