ff9f0f165108f731a15794e912a77dc02fa107b7
Alex <amaduain@gmail.com>
2022-02-08 12:26:37 +0100

    Added final options with counter

--- CHANNELS/main.go
+++ CHANNELS/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type counters map[string]int
 
 func main() {
 	links := []string{
@@ -13,16 +16,39 @@ func main() {
 		"http://stackoverflow.com",
 		"http://golang.org",
 	}
+	linkCounters := counters{
+		"http://www.google.com":    0,
+		"http://amazon.com":        0,
+		"http://www.facebook.com":  0,
+		"http://stackoverflow.com": 0,
+		"http://golang.org":        0,
+	}
+	c := make(chan string)
 	for _, link := range links {
-		checkLink(link)
+		linkCounters[link] = linkCounters[link] + 1
+		fmt.Println(link, "Check #:", linkCounters[link])
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			linkCounters[link] = linkCounters[link] + 1
+			fmt.Println(link, "Check #:", linkCounters[link])
+			checkLink(link, c)
+		}(l)
+
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "is not available!")
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- link
+
 }
